b3735bd4db5fd36fc72a9ef5997aed9ce8fb0f49
Rawan <rawanalmashad@gmail.com>
2026-04-21 22:21:34 +0200

    Add Film WS and CreateFilmRequest dto

---
+++ src/main/java/org/iti/soap/dto/CreateFilmRequest.java
@@ -0,0 +1,51 @@
+package org.iti.soap.dto;
+
+import java.math.BigDecimal;
+
+public class CreateFilmRequest {
+    private String title;
+    private String description;
+    private Short releaseYear;
+    private Short languageId;
+    private BigDecimal rentalRate;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Short getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(Short releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public Short getLanguageId() {
+        return languageId;
+    }
+
+    public void setLanguageId(Short languageId) {
+        this.languageId = languageId;
+    }
+
+    public BigDecimal getRentalRate() {
+        return rentalRate;
+    }
+
+    public void setRentalRate(BigDecimal rentalRate) {
+        this.rentalRate = rentalRate;
+    }
+}
---
+++ src/main/java/org/iti/soap/ws/FilmWebService.java
@@ -0,0 +1,60 @@
+package org.iti.soap.ws;
+
+import jakarta.jws.WebMethod;
+import jakarta.jws.WebService;
+import org.iti.soap.dto.CreateFilmRequest;
+import org.iti.soap.entity.Film;
+import org.iti.soap.entity.Language;
+import org.iti.soap.exception.LanguageNotFound;
+import org.iti.soap.factory.ServiceFactory;
+import org.iti.soap.service.FilmService;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+
+@WebService
+public class FilmWebService {
+
+    private FilmService filmService = ServiceFactory.getFilmService();
+
+    @WebMethod
+    public Film getFilmById(Short id) {
+        return filmService.findById(id);
+    }
+
+    @WebMethod
+    public List<Film> getAllFilms() {
+        return filmService.findAll();
+    }
+
+    @WebMethod
+    public Film getFilmWithLanguage(Short id) {
+        return filmService.findWithLanguage(id);
+    }
+
+    @WebMethod
+    public Film createFilm(CreateFilmRequest request) throws LanguageNotFound {
+        Film film = new Film();
+        film.setTitle(request.getTitle());
+        film.setDescription(request.getDescription());
+        film.setReleaseYear(request.getReleaseYear());
+        film.setRentalRate(request.getRentalRate());
+        film.setLastUpdate(Instant.now());
+        film.setRentalDuration((short) 3);
+        film.setReplacementCost(new BigDecimal("19.99"));
+        film.setRating("G");
+
+        Optional<Language> opt= ServiceFactory.getLanguageService().getById(request.getLanguageId());
+        film.setLanguage(opt.get());
+        opt.orElseThrow(()-> new LanguageNotFound("Language not found"));
+
+        return filmService.create(film);
+    }
+
+    @WebMethod
+    public void deleteFilm(Short id) {
+        filmService.delete(id);
+    }
+}
