2afeada83aae163ab144e6aac408a97fa3c763fc
Rawan <rawanalmashad@gmail.com>
2026-04-21 22:20:00 +0200

    Add FilmService

---
+++ src/main/java/org/iti/soap/service/FilmService.java
@@ -0,0 +1,38 @@
+package org.iti.soap.service;
+
+import org.iti.soap.dao.FilmDao;
+import org.iti.soap.dto.CreateFilmRequest;
+import org.iti.soap.entity.Film;
+import org.iti.soap.entity.Language;
+import org.iti.soap.exception.LanguageNotFound;
+import org.iti.soap.factory.ServiceFactory;
+
+import java.util.List;
+import java.util.Optional;
+
+public class FilmService {
+  private FilmDao filmDao;
+  public FilmService(FilmDao filmDao)
+  {
+      this.filmDao=filmDao;
+  }
+
+        public Film create(Film film)  {
+            return filmDao.save(film);
+        }
+        public Film findById(Short id) {
+           return  filmDao.findById(id).orElseThrow(()-> new RuntimeException("Film not found "));
+        }
+        public List<Film> findAll(){
+           return filmDao.findAll();
+        }
+        public void delete(Short id) {
+           Film film=filmDao.findById(id).orElseThrow(()->new RuntimeException("Film not found"));
+           filmDao.delete(id);
+        }
+        public  Film findWithLanguage(Short id)
+        {
+          return filmDao.findWithLanguage(id);
+        }
+    }
+
