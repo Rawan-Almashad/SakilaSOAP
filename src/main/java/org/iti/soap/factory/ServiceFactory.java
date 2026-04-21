94e34aa8e424a02d8bac1113bc272dc5c2f1b3f6
Rawan <rawanalmashad@gmail.com>
2026-04-21 22:21:08 +0200

    Add LanguageService in factory

--- src/main/java/org/iti/soap/factory/ServiceFactory.java
+++ src/main/java/org/iti/soap/factory/ServiceFactory.java
@@ -1,13 +1,20 @@
 package org.iti.soap.factory;
 
+import org.iti.soap.dao.FilmDao;
 import org.iti.soap.dao.LanguageDao;
+import org.iti.soap.service.FilmService;
 import org.iti.soap.service.LanguageService;
 
 public class ServiceFactory {
     private static final LanguageService languageService =
             new LanguageService(new LanguageDao());
+    private static final FilmService filmService =
+            new FilmService(new FilmDao());
 
     public static LanguageService getLanguageService() {
         return languageService;
+    }
+    public static FilmService getFilmService() {
+        return filmService;
     }
 }
