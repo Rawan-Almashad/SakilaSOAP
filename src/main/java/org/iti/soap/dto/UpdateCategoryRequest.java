e3a33b253984abb81e2edc83a7d05a984058e767
Rawan <rawanalmashad@gmail.com>
2026-04-24 17:39:27 +0300

    Add Category dto

---
+++ src/main/java/org/iti/soap/dto/UpdateCategoryRequest.java
@@ -0,0 +1,23 @@
+package org.iti.soap.dto;
+
+
+public class UpdateCategoryRequest {
+    private Short categoryId;
+    private String name;
+
+    public Short getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Short categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
