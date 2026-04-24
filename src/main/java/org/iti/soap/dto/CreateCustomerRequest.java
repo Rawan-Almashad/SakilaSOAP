4cba1fd87b16067d274f96859d3f58109fd6e6c8
Rawan <rawanalmashad@gmail.com>
2026-04-24 17:35:27 +0300

    Add CustomerDtos

---
+++ src/main/java/org/iti/soap/dto/CreateCustomerRequest.java
@@ -0,0 +1,59 @@
+package org.iti.soap.dto;
+
+
+public class CreateCustomerRequest {
+    private Short storeId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Short addressId;
+    private Boolean active;
+
+    public Short getStoreId() {
+        return storeId;
+    }
+
+    public void setStoreId(Short storeId) {
+        this.storeId = storeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Short getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(Short addressId) {
+        this.addressId = addressId;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+}
---
+++ src/main/java/org/iti/soap/dto/CustomerResponse.java
@@ -0,0 +1,159 @@
+package org.iti.soap.dto;
+
+public class CustomerResponse {
+    private Short id;
+    private Short storeId;
+    private String storeAddress;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Short addressId;
+    private String address;
+    private String addressDistrict;
+    private String phone;
+    private Short cityId;
+    private String city;
+    private Short countryId;
+    private String country;
+    private Boolean active;
+    private String createDate;
+    private String lastUpdate;
+
+    public CustomerResponse() {}
+
+    public Short getId() {
+        return id;
+    }
+
+    public void setId(Short id) {
+        this.id = id;
+    }
+
+    public Short getStoreId() {
+        return storeId;
+    }
+
+    public void setStoreId(Short storeId) {
+        this.storeId = storeId;
+    }
+
+    public String getStoreAddress() {
+        return storeAddress;
+    }
+
+    public void setStoreAddress(String storeAddress) {
+        this.storeAddress = storeAddress;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Short getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(Short addressId) {
+        this.addressId = addressId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddressDistrict() {
+        return addressDistrict;
+    }
+
+    public void setAddressDistrict(String addressDistrict) {
+        this.addressDistrict = addressDistrict;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Short getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(Short cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Short getCountryId() {
+        return countryId;
+    }
+
+    public void setCountryId(Short countryId) {
+        this.countryId = countryId;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public String getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(String createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(String lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+}
