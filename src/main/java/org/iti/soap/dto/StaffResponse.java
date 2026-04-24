2ab203f231862c140ef3f6aa8117b69ca67aaad8
Rawan <rawanalmashad@gmail.com>
2026-04-24 17:37:22 +0300

    Add Staff DTOs

---
+++ src/main/java/org/iti/soap/dto/StaffResponse.java
@@ -0,0 +1,97 @@
+package org.iti.soap.dto;
+
+
+public class StaffResponse {
+    private Byte id;
+    private String firstName;
+    private String lastName;
+    private Short addressId;
+    private String address;
+    private String email;
+    private Short storeId;
+    private Boolean active;
+    private String username;
+    private String lastUpdate;
+
+    public StaffResponse() {}
+
+    public Byte getId() {
+        return id;
+    }
+
+    public void setId(Byte id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Short getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(Short addressId) {
+        this.addressId = addressId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Short getStoreId() {
+        return storeId;
+    }
+
+    public void setStoreId(Short storeId) {
+        this.storeId = storeId;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(String lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+}
